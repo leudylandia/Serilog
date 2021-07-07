7a9e4c22c3111de481d1f18e6c5de1f37c01085a
leudylandia <leudylandia26@hotmail.com>
2021-07-07 19:53:47 -0400

    Example Serilog with Seq

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("logs/myapp-{DATE}.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.Console()
+                .CreateLogger();
+            Environment.SetEnvironmentVariable("USERNAME", "Leudy");
+            Log.Information("Hello, world!");
+            int a = 10, b = 0;
+            Log.Debug("Dividiendo entre {A} by {B}", a, b);
+            Log.Error( "Somthing went wrong");
+            Log.Debug("Getting started");
+            Log.Information("Hello {Name} age: {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), 30);
+            Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
+
+            Log.Fatal("Fatal Error");
+            Log.Warning("Warning Error");
+            //Log.CloseAndFlush();
+
+            Console.ReadLine();
+            Console.WriteLine("Hola leudy");
+            Console.ReadLine();
+
+            Log.Information("Hello, world!");
+            Log.Debug("Dividiendo entre {A} by {B}", a, b);
+            Log.Error("Somthing went wrong");
+            Log.Debug("Getting started");
+            Log.Information("Hello {Name} age: {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), 30);
+            Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
+
+            Log.Fatal("Fatal Error");
+            Log.Warning("Warning Error");
+            Log.CloseAndFlush();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SerilogApp/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SerilogApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("~/logs/log-{Date}.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(path, rollingInterval: RollingInterval.Day)
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341/")
+                .CreateLogger();
+           
+            Environment.SetEnvironmentVariable("USERNAME", "Leudy");
+            Log.Information("Hello from Index!");
+            Log.Information("Hello from Index {Valor}", TypeLog.Information);
+            int a = 10, b = 0;
+            Log.Debug("Dividiendo entre {A} by {B}", a, b);
+            Log.Error("Somthing went wrong");
+            Log.Debug("Getting started");
+            Log.Information("Hello {Name} age: {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), 30);
+            Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
+
+            Log.Fatal("Fatal Error");
+            Log.Warning("Warning Error");
+            Log.CloseAndFlush();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public void WriteLog()
+        {
+
+        }
+    }
+
+    public enum TypeLog
+    {
+        Error,
+        Warning,
+        Information,
+        Debug,
+        Fatal
+    }
+}
